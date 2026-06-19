eb5f492713dc3826d31904ad71f1296b15c2e839
Geral_07 <geraldinesuarezch07@gmail.com>
2026-06-18 22:19:55 -0500

    encapsulacion

---
+++ src/Encapsulación (Getters y Setters).java
@@ -0,0 +1,19 @@
+class Persona {
+
+    String nombre;
+
+    Persona(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Persona1 = new Persona("Ana");
+        Persona2 = new Persona("Luis");
+
+        System.out.println(persona1.nombre);
+        System.out.println(persona2.nombre);
+    }
+}
